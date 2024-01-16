7500bdd61235e67aa634f107df2ae761baa7b885
Kate Khovanskaya <KhovanskayaK@yandex.ru>
2024-01-16 09:40:09 +0300

    commit2 server part

--- src/main/java/com/app/coffee/controller/CoffeeOrderController.java
+++ src/main/java/com/app/coffee/controller/CoffeeOrderController.java
@@ -2,20 +2,22 @@ package com.app.coffee.controller;
 
 import com.app.coffee.model.CoffeeOrder;
 import com.app.coffee.service.CoffeeService;
+import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 import java.util.function.Supplier;
-import org.apache.log4j.Logger;
 
 @RestController
 @RequestMapping("/coffeeOrder")
 public class CoffeeOrderController {
-    private static final Logger logger = Logger.getLogger(String.valueOf(CoffeeOrderController.class));
+
+    private static final Logger logger = Logger.getLogger(CoffeeOrderController.class);
 
     @Autowired
     private CoffeeService coffeeService;
@@ -46,15 +48,9 @@ public class CoffeeOrderController {
         try {
             if(coffeeService.isJSONValid(jsonCoffeeOrder.toString())) {
                 CoffeeOrder orderCreated = coffeeService.create(jsonCoffeeOrder);
-                var uri = ServletUriComponentsBuilder.fromCurrentRequest().path(travelCreated.getOrderNumber()).build().toUri();
-
-                if(travelService.isStartDateGreaterThanEndDate(travelCreated)){
-                    logger.error("The start date is greater than end date.");
-                    return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
-                }else {
-                    travelService.add(travelCreated);
-                    return ResponseEntity.created(uri).body(null);
-                }
+                var uri = ServletUriComponentsBuilder.fromCurrentRequest().path(orderCreated.getOrderNumber()).build().toUri();
+                coffeeService.add(orderCreated);
+                return ResponseEntity.created(uri).body(null);
             }else {
                 return ResponseEntity.badRequest().body(null);
             }
@@ -63,4 +59,26 @@ public class CoffeeOrderController {
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
         }
     }
+
+    @PutMapping(path = "/{id}", produces = { "application/json" })
+    public ResponseEntity<CoffeeOrder> update(@PathVariable("id") long id, @RequestBody JSONObject coffeeOrder) {
+        try {
+            if(coffeeService.isJSONValid(coffeeOrder.toString())) {
+                CoffeeOrder orderToUpdate = coffeeService.findById(id);
+                if(orderToUpdate == null){
+                    logger.error("Travel not found.");
+                    return ResponseEntity.notFound().build();
+                }else {
+                    orderToUpdate = coffeeService.update(orderToUpdate, coffeeOrder);
+                    return ResponseEntity.ok(orderToUpdate);
+                }
+            }else {
+                return ResponseEntity.badRequest().body(null);
+            }
+        }catch(Exception e) {
+            logger.error("JSON fields are not parsable." + e);
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
+        }
+    }
 }
+
